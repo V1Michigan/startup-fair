fa0e3dee8efd13dd20a235f67e47e2babaa8bcd6
Varun Jindal <vjindal0112@gmail.com>
2022-01-19 18:24:05 -0500

    added bouncing arrow

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga";
 
-const Logo = ({ src, href, alt, size }) => (
+const Logo = ({src, href, alt, size}) => (
   <a href={href} target="_blank" rel="noopener noreferrer">
     <img className={`w-${size} hover:opacity-75`} src={src} alt={alt} />
   </a>
@@ -150,6 +150,19 @@ const Header = () => (
         </p>
       </div>
       <Companies />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="#f3f3f3"
+        className="animate-bounce h-8 w-8 mt-4 md:mt-8 lg:mt-16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+          clipRule="evenodd"
+        />
+      </svg>
     </div>
   </div>
 );
