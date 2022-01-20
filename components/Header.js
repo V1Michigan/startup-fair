abb188c934a0d36936a95b5fd23b117e23f5e77e
Elliot Klein <elliotfklein@gmail.com>
2022-01-19 20:49:35 -0500

    Spread out header components

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Logo = ({ src, href, alt, size }) => (
 );
 
 const Companies = () => (
-  <div className="flex flex-col items-center justify-center gap-4 mt-6">
+  <div className="flex flex-col items-center justify-center gap-4 mt-auto">
     <div className="flex flex-row items-center justify-start gap-8">
       {/* Title sponsor */}
       <Logo
@@ -118,58 +118,56 @@ const Companies = () => (
 );
 
 const Header = () => (
-  <div className="h-screen p-8 bg-gray-800 border border-bottom bg-topography-white text-center flex items-center justify-center">
-    <div className="flex flex-col items-center justify-center">
-      <div className="my-2">
-        <h2 className="md:text-2xl text-xl mt-1 tracking-tight text-yellow-400 syncopate">
-          University of Michigan
-        </h2>
-        <h1 className="md:text-6xl text-5xl tracking-tight font-bold text-white leading-none syncopate">
-          V1 STARTUP FAIR 🚀
-        </h1>
-        <h2 className="text-xl mt-1 tracking-tight text-yellow-400 syncopate">
-          FEBRUARY 4th, 2022
-        </h2>
-      </div>
-      <div className="mt-6">
-        <a
-          href="#apply"
-          className="text-base md:text-xl font-bold outline-none text-black py-3 px-6 rounded-md bg-white hover:opacity-75"
-          onClick={() =>
-            ReactGA.event({
-              category: "Button",
-              action: "Apply",
-              label: "Apply Now (intra-page link)",
-            })
-          }
-        >
-          Apply Now ›
-        </a>
-        <p className="text-gray-300 text-sm italic font-bold mt-6 pb-2">
-          Student applications close January 22nd
-        </p>
-      </div>
-      <Companies />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        viewBox="0 0 20 20"
-        fill="#f3f3f3"
-        className="animate-bounce cursor-pointer h-8 w-8 mt-4 md:mt-8"
+  <div className="h-screen p-8 pb-2 md:pt-12 bg-gray-800 border border-bottom bg-topography-white text-center flex flex-col items-center justify-center">
+    <div className="my-2">
+      <h2 className="md:text-2xl text-xl mt-1 tracking-tight text-yellow-400 syncopate">
+        University of Michigan
+      </h2>
+      <h1 className="md:text-6xl text-5xl tracking-tight font-bold text-white leading-none syncopate">
+        V1 STARTUP FAIR 🚀
+      </h1>
+      <h2 className="text-xl mt-1 tracking-tight text-yellow-400 syncopate">
+        FEBRUARY 4th, 2022
+      </h2>
+    </div>
+    <div className="mt-6">
+      <a
+        href="#apply"
+        className="text-base md:text-xl font-bold outline-none text-black py-3 px-6 rounded-md bg-white hover:opacity-75"
         onClick={() =>
-          window.scrollTo({
-            top: window.innerHeight,
-            behavior: "smooth",
+          ReactGA.event({
+            category: "Button",
+            action: "Apply",
+            label: "Apply Now (intra-page link)",
           })
         }
       >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
-          clipRule="evenodd"
-        />
-      </svg>
+        Apply Now ›
+      </a>
+      <p className="text-gray-300 text-sm italic font-bold mt-6 pb-2">
+        Student applications close January 22nd
+      </p>
     </div>
+    <Companies />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="#f3f3f3"
+      className="animate-bounce cursor-pointer h-8 w-8 mt-4 md:mt-8"
+      onClick={() =>
+        window.scrollTo({
+          top: window.innerHeight,
+          behavior: "smooth",
+        })
+      }
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+        clipRule="evenodd"
+      />
+    </svg>
   </div>
 );
 
