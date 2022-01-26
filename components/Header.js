50347b852625585977205997e0cbccaaa83dc666
Elliot Klein <elliotfklein@gmail.com>
2022-01-25 23:13:48 -0500

    Link to company directory

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import ReactGA from "react-ga";
+import { useRouter } from "next/router";
 
 const Logo = ({ src, href, alt, size }) => (
   <a href={href} target="_blank" rel="noopener noreferrer">
@@ -153,60 +154,72 @@ const Companies = () => (
   </div>
 );
 
-const Header = () => (
-  <div className="h-max-screen-content px-8 py-6 bg-gray-800 border border-bottom bg-topography-white text-center flex justify-center">
-    <div className="flex flex-col items-center justify-center">
-      <div className="md:my-3">
-        {/* <h2 className="md:text-4xl text-lg mt-1 tracking-tight text-yellow-400 syncopate">
+const Header = () => {
+  const router = useRouter();
+  return (
+    <div className="h-max-screen-content px-8 py-6 bg-gray-800 border border-bottom bg-topography-white text-center flex justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <div className="md:my-3">
+          {/* <h2 className="md:text-4xl text-lg mt-1 tracking-tight text-yellow-400 syncopate">
           University of Michigan
         </h2> */}
-        <h1 className="md:text-7xl text-3xl tracking-tight font-bold text-white leading-none syncopate">
-          V1 STARTUP FAIR 🚀
-        </h1>
-        <h2 className="md:text-xl text-md mt-1 tracking-tight text-yellow-400 syncopate">
-          FEBRUARY 4th, 2022
-        </h2>
-      </div>
-      <div className="mt-4">
-        <a
-          // href="#apply"
-          className="text-xl md:text-2xl font-bold outline-none text-black py-3 px-6 rounded-md bg-white  /*hover:opacity-75*/ opacity-50 cursor-not-allowed"
+          <h1 className="md:text-7xl text-3xl tracking-tight font-bold text-white leading-none syncopate">
+            V1 STARTUP FAIR 🚀
+          </h1>
+          <h2 className="md:text-xl text-md mt-1 tracking-tight text-yellow-400 syncopate">
+            FEBRUARY 4th, 2022
+          </h2>
+        </div>
+        {/* <div className="mt-4"> */}
+        {/* <a
+            href="#apply"
+            className="text-xl md:text-2xl font-bold outline-none text-black py-3 px-6 rounded-md bg-white  /*hover:opacity-75 opacity-50 cursor-not-allowed"
+            onClick={() =>
+              ReactGA.event({
+                category: "Button",
+                action: "Apply",
+                label: "Apply Now (intra-page link)",
+              })
+            }
+          >
+            Apply Now ›
+          </a> */}
+        {/* <p className="text-gray-300 text-sm md:text-base italic font-bold mt-6 pb-2">
+            Student applications close January 22nd
+          </p> */}
+        {/* </div> */}
+        <div className="my-6">
+          <a
+            className="text-xl md:text-2xl font-bold outline-none text-black py-2 px-4 rounded-md bg-yellow-400 hover:opacity-75 cursor-pointer"
+            onClick={() => router.push("https://directory.v1michigan.com")}
+          >
+            Company Directory ›
+          </a>
+          <p className="text-gray-300 text-sm md:text-base italic font-bold mt-6 pb-2">
+            Student applications are now closed — please check back in the fall
+          </p>
+        </div>
+        <Companies />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="#f3f3f3"
+          className="animate-bounce cursor-pointer h-8 w-8 mt-6"
           onClick={() =>
-            ReactGA.event({
-              category: "Button",
-              action: "Apply",
-              label: "Apply Now (intra-page link)",
+            window.scrollTo({
+              top: window.innerHeight,
+              behavior: "smooth",
             })
           }
         >
-          {/* Apply Now › */}
-          Apps Closed ›
-        </a>
-        <p className="text-gray-300 text-sm md:text-base italic font-bold mt-6 pb-2">
-          Student applications close January 22nd
-        </p>
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+            clipRule="evenodd"
+          />
+        </svg>
       </div>
-      <Companies />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="#f3f3f3"
-        className="animate-bounce cursor-pointer h-8 w-8 mt-6"
-        onClick={() =>
-          window.scrollTo({
-            top: window.innerHeight,
-            behavior: "smooth",
-          })
-        }
-      >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
-          clipRule="evenodd"
-        />
-      </svg>
     </div>
-  </div>
-);
-
+  );
+};
 export default Header;
