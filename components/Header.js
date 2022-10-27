9d6112af3976e862aebd73fe8bae1aee569fabf0
HariChidam <hchidam@umich.edu>
2022-10-27 11:42:56 -0400

    added Healpay Logo

--- components/Header.js
+++ components/Header.js
@@ -28,6 +28,12 @@ const CompaniesF22 = () => (
         alt="Intros"
         size="56"
       />
+      <Logo
+        src="/companies/healpay.png"
+        href="https://www.healpay.com/"
+        alt="Healpay"
+        size="47"
+      />
     </div>
     <p className="text-gray-300 text-xs md:text-sm italic font-bold mt-2 pb-2">
       ...more companies to be announced soon!
