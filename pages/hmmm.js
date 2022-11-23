f9d8168e4fb59179741d8ae41989154d9b3965ab
Shrey Pandya <shrey150@yahoo.com>
2022-11-23 13:44:38 -0500

    remove hmmm(m)

--- pages/hmmm.js
+++
@@ -1,3 +0,0 @@
-export default function Hmmm() {
-  return <div style={{ padding: "10px" }}>It's not a bunny</div>;
-}
--- pages/hmmmm.js
+++
@@ -1,3 +0,0 @@
-export default function Hmmmm() {
-  return <div style={{ padding: "10px" }}>It's not a rabbit</div>;
-}
