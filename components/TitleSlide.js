b9ee2900721a2c846dec1cee307646113d73dc09
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2023-10-10 00:36:48 -0400

    Added Companies for F23 (#41)
    * Added Companies for F23
    
    * Updated to 400+ students

--- components/TitleSlide.js
+++ components/TitleSlide.js
@@ -1,37 +1,25 @@
 import Logo from "./Logo";
+import CompaniesF23 from "./CompaniesF23";
+import Statistic from "./Statistic";
 
 export default function TitleSlide() {
   return (
-    <div style={{ height: 'calc(100vh - 108px)' }}>
+    <div>
       <div className="h-full w-full bg-landing bg-cover bg-center bg-no-repeat">
-        <div className="h-full w-full bg-shade relative flex items-center justify-center">
-          <section className="max-w-6xl mx-auto my-4 text-center">
-            <h1 className="text-5xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-tight font-bold">
+        <div className="h-full w-full bg-shade py-4">
+          <section className="max-w-6xl mx-auto my-4">
+            <h1 className="text-5xl text-white text-center tracking-tight font-bold">
               <span className="text-yellow-500">V1</span> Startup Fair
             </h1>
-            <p className="mt-4 mb-6 text-xl md:text-3xl lg:text-4xl text-gray-400 tracking-tight font-bold"> 
+            <p className="mt-4 mb-8 text-lg text-gray-400 text-center tracking-tight font-bold">
               October 12th, 2023
             </p>
+            <div className="flex flex-row flex-wrap justify-center gap-16 my-16">
+              <Statistic value={17} label="high-growth startups" />
+              <Statistic value={`${400}+`} label="top students" />
+            </div>
+            <CompaniesF23 />
           </section>
-
-          {/* Absolute positioning to place this section at the bottom */}
-          <div className="absolute bottom-5 left-0 right-0 flex flex-wrap gap-x-1 items-center justify-center mt-4 p-4"> 
-            <p className="text-gray-300 text-xs md:text-base font-bold italic" style={{marginRight: '1.0rem'}}>
-              In partnership with 
-            </p>
-            <Logo
-              src="/companies/BusinessTechRoss.png"
-              href="https://businesstech.bus.umich.edu/"
-              alt="Michigan Ross Business + Tech"
-              size="35"
-            />
-            <Logo
-              src="/companies/ZLIwhite.png"
-              href="https://zli.umich.edu/"
-              alt="Zell Lurie Institute"
-              size="48"
-            />
-          </div>
         </div>
       </div>
     </div>
